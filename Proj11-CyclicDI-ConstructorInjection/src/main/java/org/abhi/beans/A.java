7f73d42faebeb563c0328863b5654998a81c9a5f
Dev2 <Dev2@gmail.com>
2020-12-24 16:54:41 +0530

    commit

---
+++ Proj11-CyclicDI-ConstructorInjection/src/main/java/org/abhi/beans/A.java
@@ -0,0 +1,18 @@
+package org.abhi.beans;
+
+public class A {
+	private B b;
+	
+	public A(B b) {
+		System.out.println("A() :: 1- param constructor ");
+		this.b = b;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "A [b=" + b + "]";
+	}
+	
+	
+}
